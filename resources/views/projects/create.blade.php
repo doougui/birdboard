b689ddc3930e371bccb02010de2822485b338173
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-03-01 18:56:44 -0300

    Updating project creation form

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,33 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-    <form method="POST" action="/projects">
+    <form
+        method="POST"
+        action="/projects"
+        class="lg/w-1/2 lg:mx-auto bg-white p-6 md:py-12 md:px-16 rounded shadow"
+    >
         @csrf
 
-        <h1 class="heading is-1">Create a project</h1>
+        <h1 class="text-2xl font-normal mb-10 text-center">
+            Create a project
+        </h1>
 
-        <div class="field">
-            <label for="title" class="label">Title</label>
+        <div class="field mb-6">
+            <label for="title" class="label text-sm mb-2 block">Title</label>
 
             <div class="control">
-                <input type="text" class="input" name="title" placeholder="Title">
+                <input
+                    type="text"
+                    class="input bg-transparent border border-gray-light rounded p-2 text-xs w-full"
+                    name="title"
+                    placeholder="My next awesome project"
+                >
+            </div>
+        </div>
+
+        <div class="field mb-6">
+            <label class="label text-sm mb-2 block" for="description">Description</label>
+
+            <div class="control">
+                    <textarea
+                        name="description"
+                        rows="10"
+                        class="textarea bg-transparent border border-gray-light rounded p-2 text-xs w-full"
+                        placeholder="I should start learning piano."
+                        required></textarea>
             </div>
         </div>
 
         <div class="field">
-            <label for="description" class="label">Description</label>
-
             <div class="control">
-                <textarea name="description" id="description" cols="30" rows="10" class="textarea"></textarea>
+                <button type="submit" class="button is-link mr-2">Create Project</button>
+
+                <a href="/projects" class="text-default">Cancel</a>
             </div>
         </div>
-
-        <div class="field">
-            <div class="control">
-                <button type="submit" class="button is-link">Create Project</button>
-                <a href="/projects">Cancel</a>
-            </div>
-        </div>
-
     </form>
 @endsection
