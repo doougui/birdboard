aa21c0db68f6fe2b4a26efd5ecc81d33e1b6c0a3
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-03-09 17:51:29 -0300

    Showing invitations form and using error bags to handle errors on multiple forms

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -73,19 +73,34 @@
                         <button type="submit" class="button">Save</button>
                     </form>
 
-                    @if($errors->any())
-                        <div class="field mt-6">
-                                @foreach ($errors->all() as $error)
-                                    <li class="text-sm text-red-500">{{ $error }}</li>
-                                @endforeach
-                        </div>
-                    @endif
+                    @include('errors')
                 </div>
             </div>
 
             <div class="lg:w-1/4 px-3">
                 @include('projects.card')
                 @include('projects.activity.card')
+
+                <div class="card flex flex-col mt-3">
+                    <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-light pl-4">
+                        Invite a user
+                    </h3>
+
+                    <form method="POST" action="{{ $project->path() . '/invitations' }}">
+                        @csrf
+                        <div class="mb-3">
+                            <input type="email"
+                                   name="email"
+                                   id="email"
+                                   class="input bg-transparent border border-muted-light rounded p-2 text-xs w-full{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                                    placeholder="Email address"
+                            >
+                        </div>
+                        <button type="submit" class="button">Invite</button>
+
+                        @include('errors', ['bag' => 'invitations'])
+                    </form>
+                </div>
             </div>
         </div>
     </main>
