cac87622042a1b7e8d576f1dab152213464b25e8
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-03-09 19:54:14 -0300

    Refactoring project creation test

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -24,6 +24,7 @@ class ProjectFactory extends Factory
     {
         return [
             'title' => $this->faker->sentence(4),
+            'notes' => 'Foobar notes',
             'description' => $this->faker->sentence(4),
             'owner_id' => User::factory()
         ];
